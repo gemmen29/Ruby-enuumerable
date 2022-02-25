258c71f43f59189cbb486d47f9c4277d1a2f2d5e
ShadyShawkat <shadyshawkat91@gmail.com>
2022-02-25 17:50:34 +0200

    Add each method to MyList class

--- my_list.rb
+++ my_list.rb
@@ -1,4 +1,4 @@
-require_relative './MyEnumerable'
+require_relative 'my_enumerable'
 
 class MyList
   include MyEnumerable
@@ -6,4 +6,13 @@ class MyList
   def initialize (*list)
     @list = list
   end
+
+  def each
+    @list.each do |num| 
+      yield num
+    end
+  end
 end
+
+list = MyList.new(2, 3, 4, 5)
+p list.all? {|e| e <= 5}
