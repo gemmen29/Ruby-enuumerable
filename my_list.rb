234e0ccec926d3bbc0b28fecb3467c507332da12
George Magdy <georgtriple@gmail.com>
2022-02-25 17:54:59 +0200

    Add test cases for MyList

--- my_list.rb
+++ my_list.rb
@@ -15,4 +15,8 @@ class MyList
 end
 
 list = MyList.new(2, 3, 4, 5)
-p list.all? {|e| e <= 5}
+p list.all? {|e| e <= 5} # true
+p list.all? {|e| e >= 5} # false
+p list.any? {|e| e >= 5} # true
+p list.any? {|e| e > 5} # false
+p list.filter {|e| e > 3} # [4, 5]
